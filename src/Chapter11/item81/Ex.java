0d0ec8a0f210ae1f1de749485f3a9ff76edb5f76
limwoobin <drogba02@naver.com>
2020-11-04 16:05:55 +0900

    Effective java
    동시성
    - wait , notify 보다는 동시성 유틸리티 사용.

---
+++ src/Chapter11/item81/Ex.java
@@ -0,0 +1,39 @@
+package Chapter11.item81;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+
+public class Ex {
+    // 동시 실행 시간을 재는 간단한 프레임워크
+    public static long time(Executor executor , int concurrency , Runnable action) throws InterruptedException {
+        CountDownLatch ready = new CountDownLatch(concurrency);
+        CountDownLatch start = new CountDownLatch(1);
+        CountDownLatch done = new CountDownLatch(concurrency);
+
+        for (int i=0; i<concurrency; i++) {
+            executor.execute(() -> {
+                // 타이머에게 준비를 마쳤음을 알린다.
+                ready.countDown();
+                try {
+                    // 모든 작업자 스레드가 준비될때까지 기다린다.
+                    start.await();
+                    action.run();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                } finally {
+                    done.countDown();
+                }
+            });
+        }
+
+        ready.await(); // 모든 작업자가 준비될때까지 기다린다.
+        long startNanos = System.nanoTime();
+        start.countDown();      // 작업자들을 깨운다.
+        done.await();       // 모든 작업자가 일을 마치기를 기다린다.
+        return System.nanoTime() - startNanos;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
