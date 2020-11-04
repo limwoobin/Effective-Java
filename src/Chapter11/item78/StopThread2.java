473bdbc0c4fa7d0b2e29d9ba395d652e57377435
limwoobin <drogba02@naver.com>
2020-11-04 14:27:29 +0900

    Effective java
    동시성
    - 공유중인 가변 데이터는 동기화해 사용하라.

---
+++ src/Chapter11/item78/StopThread2.java
@@ -0,0 +1,30 @@
+package Chapter11.item78;
+
+import java.util.concurrent.TimeUnit;
+
+// 적절히 동기화해 스레드가 정상적으로 종료
+public class StopThread2 {
+    private static boolean stopRequested;
+
+    private static synchronized void requestStop() {
+        stopRequested = true;
+    }
+
+    private static synchronized boolean isStopRequested() {
+        return stopRequested;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread backgroundThread = new Thread(() -> {
+            int i = 0;
+            while (!isStopRequested()) {
+                i++;
+            }
+        });
+
+        backgroundThread.start();
+
+        TimeUnit.SECONDS.sleep(1);
+        requestStop();
+    }
+}
