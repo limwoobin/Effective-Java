b22b96e795dcfe0038043c7c2113bf8bba4984ab
limwoobin <drogba02@naver.com>
2020-11-04 18:00:34 +0900

    Effective java
    동시성
    - 프로그램의 동작을 스레드 스케줄러에 기대지 말자.

---
+++ src/Chapter11/item84/SlowCountDownLatch.java
@@ -0,0 +1,29 @@
+package Chapter11.item84;
+
+// 끔찍한 CountDownLacth 구현 - 바쁜 대기 버전!
+public class SlowCountDownLatch {
+    private int count;
+
+    public SlowCountDownLatch(int count) {
+        if (count < 0) {
+            throw new IllegalArgumentException(count + " < 0");
+        }
+        this.count = count;
+    }
+
+    public void await() {
+        while (true) {
+            synchronized (this) {
+                if (count == 0) {
+                    return;
+                }
+            }
+        }
+    }
+
+    public synchronized void countDown() {
+        if (count != 0) {
+            count--;
+        }
+    }
+}
