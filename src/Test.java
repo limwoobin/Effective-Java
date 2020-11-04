0cfca2efc2b2367ad0174d5c37d138c5888ac46c
limwoobin <drogba02@naver.com>
2020-11-04 17:46:32 +0900

    Effective java
    동시성
    - 지연 초기화는 신중히 사용하라.

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,46 @@
-import java.util.Arrays;
-import java.util.stream.Stream;
+
 
 public class Test {
     public static void main(String[] args) {
-        String[][] arrays = new String[][]{ {"a1", "a2"}, {"b1", "b2"}, {"c1", "c2", "c3"} };
+        Runnable runnable = new RunnableTest2();
+        Thread thread = new Thread(runnable);
+        Thread thread2 = new Thread(runnable);
 
-        Stream<String[]> stream4 = Arrays.stream(arrays);
-
-        Stream<String> stream5 = stream4.flatMap(s -> Arrays.stream(s));
-
-        stream5.forEach(System.out::println);
+        thread.start();
+        thread2.start();
     }
 }
+
+class Account_2 {
+    int balance = 1000;
+
+    public synchronized void withdraw(int money) {
+        while (balance < money) {
+            try {
+                wait();
+            } catch (Exception e) {
+            }
+        }
+        balance -= money;
+    }
+
+    public synchronized void deposit(int money) {
+        balance += money;
+        notify();
+    }
+}
+
+class RunnableTest2 implements Runnable {
+    Account_2 account = new Account_2();
+
+    public void run() {
+        while (account.balance > 0) {
+            int money = (int) (Math.random() * 3 + 1) * 100;
+            account.withdraw(money);
+            System.out.println("balance : " + account.balance);
+        }
+        account.deposit(1000);
+    }
+}
+
+
