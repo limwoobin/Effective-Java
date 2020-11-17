d81f13556a7c88c7bd5bcdc63abadeb0b982363d
limwoobin <drogba02@naver.com>
2020-11-17 15:48:04 +0900

    Effective java
    - 정적 팩터리 메소드 예제

---
+++ src/Cycle2/Item1/Child.java
@@ -0,0 +1,33 @@
+package Cycle2.Item1;
+
+public class Child {
+    private String name;
+    private Sex sex;
+
+    public Child(String name , Sex sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    public static Child son(String name) {
+        return new Child(name , Sex.MALE);
+    }
+
+    public static Child daughter(String name) {
+        return new Child(name , Sex.FEMALE);
+    }
+
+    private enum Sex {
+        MALE , FEMALE
+    }
+
+    @Override
+    public String toString() {
+        return "Child{" +
+                "name='" + name + '\'' +
+                ", sex=" + sex +
+                '}';
+    }
+}
+
+
---
+++ src/Cycle2/Item1/Main.java
@@ -0,0 +1,11 @@
+package Cycle2.Item1;
+
+public class Main {
+    public static void main(String[] args) {
+        Child mySon = Child.son("mySon");
+        Child myDaughter = Child.daughter("myDaughter");
+
+        System.out.println(mySon.toString());
+        System.out.println(myDaughter.toString());
+    }
+}
