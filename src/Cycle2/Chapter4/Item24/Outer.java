1765541e99e5a061f4c996b4a34959f4919eb576
limwoobin <drogba02@naver.com>
2020-11-20 17:10:24 +0900

    Effective java
    - 멤버클래스는 되도록 static으로 만들어라

---
+++ src/Cycle2/Chapter4/Item24/Outer.java
@@ -0,0 +1,12 @@
+package Cycle2.Chapter4.Item24;
+
+public final class Outer {
+
+
+    static class Inner {
+
+        void printValue() {
+            System.out.println("print Value~");
+        }
+    }
+}
