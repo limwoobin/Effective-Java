54627e5c04754581c99f06561917f7077f7baee1
limwoobin <drogba02@naver.com>
2020-11-20 16:28:29 +0900

    Effective java
    - 인터페이스는 타입을 정의하는 용도로만 사용하라

---
+++ src/Cycle2/Chapter4/Item22/PhysicalConstants2.java
@@ -0,0 +1,12 @@
+package Cycle2.Chapter4.Item22;
+
+// 상수 유틸리티 클래스
+public class PhysicalConstants2 {
+    private PhysicalConstants2() {} // 인스턴스화 방지
+
+    public static final double AVOGADROS_NUMBER = 6.022_140_857e23;
+
+    public static final double BOLTZMANN_CONSTANT = 1.380_648_52e-23;
+
+    public static final double ELECTRON_MASS = 9.109_383_56e-31;
+}
